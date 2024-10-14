8aa850a2520ee21bbf536fc31806b65bc3d7a068
Brian Bravol <lumalabravo@gmail.com>
2024-10-14 17:10:19 +0300

    finished get request method in fast api

--- project2-books/books.py
+++ project2-books/books.py
@@ -19,14 +19,34 @@ async def read_all_books():
     return BOOKS
 
 
-
 @app.get('/books/mybook')
 async def read_all_books():
     return {'book_title':'my favorite book!'}
 
+# path parameters dynamic
 @app.get('/books/{book_title}')
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+
+# query parameters by category filter data based on the url provided.
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return  books_to_return
+
+# query parameters by author and category filter data based on the url provided.
+
+@app.get('/book/{book_author}/')
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (book.get('author').casefold() == book_author.casefold() and
+                book.get('category').casefold() == category.casefold()):
+            books_to_return.append(book)
+    return  books_to_return
