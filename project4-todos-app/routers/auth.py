db8b782cf3b6b2e514dbb61f41bae8ebd32cf1c2
Brian Bravol <lumalabravo@gmail.com>
2024-10-15 18:48:07 +0300

    JWT(JSON Web Token)

--- project4-todos-app/routers/auth.py
+++ project4-todos-app/routers/auth.py
@@ -6,6 +6,7 @@ from models import  Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from starlette import  status
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 
@@ -32,6 +33,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+# FUNCTIONS
+# authenticate user
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user( db: db_dependency, user: UserRequest):
@@ -47,3 +58,11 @@ async def create_user( db: db_dependency, user: UserRequest):
 
     db.add(user_model)
     db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,Depends()], db:db_dependency):
+
+    user = authenticate_user(form_data.username,form_data.password,db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful authentication'
