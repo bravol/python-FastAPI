1f49f99845d5f4531bc1a1ed1ab9af57bc8dc1f5
Brian Bravol <lumalabravo@gmail.com>
2024-10-16 11:12:54 +0300

    returning json web token

--- project4-todos-app/routers/auth.py
+++ project4-todos-app/routers/auth.py
@@ -1,5 +1,7 @@
+from datetime import timedelta, datetime, timezone
 from typing import Annotated
 from fastapi import APIRouter, Depends
+from jose.constants import ALGORITHMS
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from models import  Users
@@ -7,10 +9,14 @@ from passlib.context import CryptContext
 from database import SessionLocal
 from starlette import  status
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 
 
 router = APIRouter()
+
+SECRET_KEY = '00cf6789e9cf6ecf780bf12b6117376ff91d8738c7008960481def44977d9bf8'
+ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -22,6 +28,9 @@ class UserRequest(BaseModel):
     password: str
     role: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 def get_db():
@@ -41,7 +50,14 @@ def authenticate_user(username: str, password: str, db: db_dependency):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
+
+# create access token
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
@@ -59,10 +75,12 @@ async def create_user( db: db_dependency, user: UserRequest):
     db.add(user_model)
     db.commit()
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,Depends()], db:db_dependency):
 
     user = authenticate_user(form_data.username,form_data.password,db)
     if not user:
         return 'Failed Authentication'
-    return 'Successful authentication'
+
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {"access_token": token, "token_type": 'bearer' }
