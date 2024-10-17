a3523fa2541cbcbe1b5053e9c6a37279857009c3
Brian Bravol <lumalabravo@gmail.com>
2024-10-17 17:31:45 +0300

    reusabilty in testing

--- project8-pytest/test/test_todos.py
+++ project8-pytest/test/test_todos.py
@@ -1,61 +1,14 @@
-from sqlalchemy import create_engine, StaticPool,text
-from sqlalchemy.orm import sessionmaker
-from database import Base
-from main import app
 from routers.auth import get_current_user
 from routers.todos import get_db
-from fastapi.testclient import TestClient
 from fastapi import status
-import pytest
-from models import Todos
+from .utils import *
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./testdb.db'
-
-engine = create_engine(
-SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False},
-    poolclass=StaticPool,
-
-)
-
-TestingSessionLocal =  sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def override_get_current_user():
-    return {'username': 'bravol', 'id':1, 'user_role': 'admin'}
 
 #overriding the original db, and current user
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
-client = TestClient(app)
 
-@pytest.fixture
-def test_todo():
-    todo = Todos(
-        id = 1,
-        title='Learn to code!',
-        description='Need to learn everyday',
-        priority=5,
-        complete=False,
-        owner_id=1
-    )
-
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield todo
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos"))
-        connection.commit()
 
 def test_read_all_authenticated(test_todo):
     response = client.get("/")
@@ -145,4 +98,3 @@ def test_delete_todo_not_found(test_todo):
     response = client.delete("/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert  response.json() == {'detail': 'Todo not found.'}
-    
