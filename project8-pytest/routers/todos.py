357fb147e8c16262f7626c0c2a8f01b3f48241ab
Brian Bravol <lumalabravo@gmail.com>
2024-10-18 13:22:42 +0300

    important updates made

--- project8-pytest/routers/todos.py
+++ project8-pytest/routers/todos.py
@@ -7,7 +7,10 @@ from database import SessionLocal
 from starlette import  status
 from routers.auth import get_current_user
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/todos',
+    tags=['todos']
+)
 
 
 def get_db():
@@ -36,7 +39,7 @@ async  def read_all(user:user_dependency, db:db_dependency):
 
 
 
-@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+@router.get("/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo( user: user_dependency, db:db_dependency, todo_id: int = Path(gt=0) ):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='authentication failed')
@@ -46,7 +49,7 @@ async def read_todo( user: user_dependency, db:db_dependency, todo_id: int = Pat
     raise HTTPException(status_code=404,detail='Todo not found')
 
 # create todo_model
-@router.post("/todo", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_todo(db:db_dependency, user: user_dependency, todo_request: TodoRequest):
 
     if user is None:
@@ -56,7 +59,7 @@ async def create_todo(db:db_dependency, user: user_dependency, todo_request: Tod
     db.add(todo_model)
     db.commit()
 
-@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.put("/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async  def update_todo( user:user_dependency, db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='authentication failed')
@@ -71,7 +74,7 @@ async  def update_todo( user:user_dependency, db: db_dependency, todo_request: T
     db.add(todo_model)
     db.commit()
 
-@router.delete("/todo/{todo_id}")
+@router.delete("/{todo_id}")
 async def delete_todo(user:user_dependency, db:db_dependency, todo_id: int = Path(gt=0)):
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='authentication failed')
--- project8-pytest/test/test_todos.py
+++ project8-pytest/test/test_todos.py
@@ -11,7 +11,7 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 
 def test_read_all_authenticated(test_todo):
-    response = client.get("/")
+    response = client.get("/todos")
     assert response.status_code == status.HTTP_200_OK
 
     data = response.json()[0]
@@ -26,13 +26,13 @@ def test_read_all_authenticated(test_todo):
 
 
 def test_read_one_authenticated(test_todo):
-    response = client.get("/todo/1")
+    response = client.get("/todos/1")
     assert response.status_code == status.HTTP_200_OK
     assert response.json()  == {'title':'Learn to code!','description':'Need to learn everyday',
                                  'priority':5,'complete':False,'owner_id':1, 'id': 1}
 
 def test_read_one_authenticated_not_found():
-    response = client.get("/todo/999")
+    response = client.get("/todos/999")
     assert response.status_code == 404
     assert response.json() =={'detail': 'Todo not found'}
 
@@ -46,7 +46,7 @@ def test_create_todo(test_todo):
         "complete": False
     }
 
-    response = client.post('/todo/', json=requested_data)
+    response = client.post('/todos/', json=requested_data)
     assert response.status_code == 201
 
     db =TestingSessionLocal()
@@ -65,7 +65,7 @@ def test_update_todo(test_todo):
         'complete': False
     }
 
-    response = client.put("/todo/1", json=request_data)
+    response = client.put("/todos/1", json=request_data)
     assert response.status_code == 204
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 1).first()
@@ -81,13 +81,13 @@ def test_update_todo_not_found(test_todo):
         'complete': False
     }
 
-    response = client.put("/todo/999", json=request_data)
+    response = client.put("/todos/999", json=request_data)
     assert response.status_code == 404
     assert response.json()  == {'detail': 'Todo not found.'}
 
 
 def test_delete_todo(test_todo):
-    response = client.delete("/todo/1")
+    response = client.delete("/todos/1")
     assert response.status_code == status.HTTP_200_OK
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 1).first()
@@ -95,6 +95,6 @@ def test_delete_todo(test_todo):
 
 
 def test_delete_todo_not_found(test_todo):
-    response = client.delete("/todo/999")
+    response = client.delete("/todos/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert  response.json() == {'detail': 'Todo not found.'}
