84f5491cd9250cd7806334e69cd83a96d9e74a13
Brian Bravol <lumalabravo@gmail.com>
2024-10-18 16:04:37 +0300

    building UI for user using Jinja2 templates, css and Javascript

--- project9-fullstack-app/main.py
+++ project9-fullstack-app/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import models
 from database import engine
 from routers import auth,todos, admin, users
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def test(request:Request):
+    return templates.TemplateResponse("home.html", {"request":request})
 
 
 @app.get("/healthy")
