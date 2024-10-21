03fe6fac37eab399292f81378bc3a21e36d55d0b
Brian Bravol <lumalabravo@gmail.com>
2024-10-21 18:07:41 +0300

    updates made

--- project9-fullstack-app/routers/admin.py
+++ project9-fullstack-app/routers/admin.py
@@ -1,6 +1,5 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Path
-from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from models import Todos
 from database import SessionLocal
